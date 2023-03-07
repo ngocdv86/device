675d66f08c953ee896a8d421b4a92c61e1b193a1
ngocdv <ngocdv@ghn.vn>
2023-03-07 10:29:02 +0700

    add https

--- main.go
+++ main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os/exec"
 	"runtime"
-	"strings"
 	"time"
 )
 
@@ -21,7 +19,6 @@ const (
 
 var (
 	mainMACAddress string
-	serialNumber   string
 	deviceToken    DeviceToken
 	URL            = "https://dev-online-gateway.ghn.vn/sso-v2/public-api/staff/gen-device-token"
 )
@@ -30,6 +27,7 @@ type DeviceToken struct {
 	Token       string    `json:"device_token"`
 	ExpiredTime time.Time `json:"expired_time"`
 }
+
 type GenDeviceTokenResponse struct {
 	Code    int         `json:"code"`
 	Message string      `json:"message"`
@@ -45,7 +43,7 @@ func getMainMacAddress() (string, error) {
 	//Debug
 	for _, iface := range interfaces {
 		if iface.Flags&net.FlagUp != 0 && iface.HardwareAddr.String() != "" {
-			fmt.Println(iface.Name, iface.HardwareAddr.String(), iface.HardwareAddr[0]&2 == 2)
+			fmt.Println("[debug]", iface.Name, iface.HardwareAddr.String(), iface.HardwareAddr[0]&2 == 2)
 		}
 	}
 
@@ -80,66 +78,6 @@ func getMainMacAddress() (string, error) {
 	return mainMACAddress, nil
 }
 
-func getSerialNumber() (string, error) {
-	var cmd *exec.Cmd
-	var out []byte
-	var err error
-
-	switch GOOS {
-	case "windows":
-		cmd = exec.Command("wmic", "bios", "get", "serialnumber")
-		out, err = cmd.Output()
-		if err != nil {
-			return "", err
-		}
-
-		serial := strings.Split(string(out), "\n")[1]
-		if len(serial) == 0 {
-			return "", errors.New("failed to get serial number")
-		}
-
-		return strings.TrimSpace(serial), nil
-
-	case "darwin":
-		cmd = exec.Command("system_profiler", "SPHardwareDataType")
-		out, err = cmd.Output()
-		if err != nil {
-			return "", err
-		}
-
-		serial := ""
-		for _, line := range strings.Split(string(out), "\n") {
-			if strings.Contains(line, "Serial Number") {
-				serial = strings.TrimSpace(strings.Split(line, ":")[1])
-				break
-			}
-		}
-
-		if len(serial) == 0 {
-			return "", errors.New("failed to get serial number")
-		}
-
-		return serial, nil
-
-	case "linux":
-		cmd = exec.Command("sudo", "dmidecode", "-s", "system-serial-number")
-		out, err = cmd.Output()
-		if err != nil {
-			return "", err
-		}
-
-		serial := strings.TrimSpace(string(out))
-		if len(serial) == 0 {
-			return "", errors.New("failed to get serial number")
-		}
-
-		return serial, nil
-
-	default:
-		return "", errors.New("unsupported platform")
-	}
-}
-
 func requestDeviceToken(deviceID string) (response DeviceToken, err error) {
 	requestBody, err := json.Marshal(map[string]any{
 		"device_id": deviceID,
@@ -163,8 +101,36 @@ func requestDeviceToken(deviceID string) (response DeviceToken, err error) {
 	return rs.Data, nil
 }
 
+func setup() error {
+	if _, err := exec.Command("brew", "install", "mkcert").Output(); err != nil {
+		return err
+	}
+
+	if _, err := exec.Command("mkcert", "-install").Output(); err != nil {
+		return err
+	}
+
+	if _, err := exec.Command("mkcert", "-cert-file", "cert.pem", "-key-file", "key.pem", "localhost").Output(); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
+	fmt.Println("Setup....")
+	if err := setup(); err != nil {
+		fmt.Printf("Setup error: %v\n", err)
+	}
 	fmt.Printf("Running in localhost:%d\n", PORT)
+
+	var err error
+	mainMACAddress, err = getMainMacAddress()
+	if err != nil {
+		fmt.Printf("[error] Cannot get MAC address: %v\n", err)
+		fmt.Println("[info] Không thể lấy device_id, liên hệ với IT để xử lí.")
+	} else {
+		fmt.Println("[info] device_id: ", mainMACAddress)
+	}
 
 	http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -172,21 +138,6 @@ func main() {
 		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
 
 		var err error
-		if mainMACAddress == "" {
-			mainMACAddress, err = getMainMacAddress()
-			if err != nil {
-				fmt.Printf("Get main MAC address err: %v\n", err)
-			}
-		}
-
-		if serialNumber == "" {
-			serialNumber, err = getSerialNumber()
-			if err != nil {
-				fmt.Printf("Get Serial number err: %v\n", err)
-			}
-		}
-
-		fmt.Println(mainMACAddress, serialNumber)
 		if deviceToken.Token == "" || deviceToken.ExpiredTime.Before(time.Now()) {
 			deviceToken, err = requestDeviceToken(mainMACAddress)
 			if err != nil {
@@ -203,5 +154,8 @@ func main() {
 		w.Write(jsonBytes)
 	})
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil))
+	err = http.ListenAndServeTLS(fmt.Sprintf(":%d", PORT), "cert.pem", "key.pem", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
